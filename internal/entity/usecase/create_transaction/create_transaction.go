f90b5ae7643682497054c5381881246255cbd29f
Nicollas <nicollasmk9@gmail.com>
2025-05-09 22:39:09 -0300

    database file criada

---
+++ internal/entity/usecase/create_transaction/create_transaction.go
@@ -0,0 +1,49 @@
+package createtransaction
+
+import (
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/entity"
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/entity/gateway"
+)
+
+type CreateTransactionInputDTO struct {
+	AccountIDFrom string
+	AccountIDTo   string
+	Amount        float64
+}
+
+type CreateTransactionOutputDTO struct {
+	ID string
+}
+
+type CreateTransactionUseCase struct {
+	AccountGateway  gateway.AccountGateway
+	TransactionGateway gateway.TransactionGateway
+}
+
+func NewCreateTransactionUseCase(transactionGateway gateway.TransactionGateway, accountGateway gateway.AccountGateway) *CreateTransactionUseCase {
+	return &CreateTransactionUseCase{
+		TransactionGateway: transactionGateway,
+		AccountGateway:     accountGateway,
+	}
+}
+
+func (uc *CreateTransactionUseCase) Execute(input CreateTransactionInputDTO) (*CreateTransactionOutputDTO, error) {
+	accountFrom, err := uc.AccountGateway.FindByID(input.AccountIDFrom)
+	if err != nil {
+		return nil, err
+	}
+	accountTo, err := uc.AccountGateway.FindByID(input.AccountIDTo)	
+	if err != nil {
+		return nil, err
+	}
+	transaction, err := entity.NewTransaction(accountFrom, accountTo, input.Amount)
+	if err != nil {
+		return nil, err
+	}
+	err = uc.TransactionGateway.Create(transaction)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateTransactionOutputDTO{ID: transaction.ID}, nil
+
+}
